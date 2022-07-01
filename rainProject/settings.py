5339c75f8b56b47c461081fc504d6dc0a293e8da
radguyjames <39416499+radguyjames@users.noreply.github.com>
2022-07-01 02:57:54 -0500

    Update settings.py

--- rainProject/settings.py
+++ rainProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oi!+c1#$h3$&d_maqhr)rsx^dl_^6bz6x^uzzq@sg3spjkg8%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
